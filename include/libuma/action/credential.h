3517d033a84680351361a5f5d8267b2c3a4554d6
Theowhy <theowhy@shizoku.fr>
2017-05-10 01:14:44 +0200

    Add actions

---
+++ include/libuma/action/credential.h
@@ -0,0 +1,6 @@
+#ifndef UMA_ACTION_CREDENTIAL_H
+#define UMA_ACTION_CREDENTIAL_H
+
+void uma_credential_set(Mooltipass mooltipass, const char *service, const char *login, const char *pasword);
+
+#endif //UMA_ACTION_CREDENTIAL_H
---
+++ include/libuma/action/device.h
@@ -0,0 +1,8 @@
+#ifndef UMA_ACTION_DEVICE_H
+#define UMA_ACTION_DEVICE_H
+
+#include <libuma/device/handler.h>
+
+void uma_version_get();
+
+#endif //UMA_ACTION_DEVICE_H
